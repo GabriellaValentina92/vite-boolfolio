a2306f4f9668064a7669ad93bba9bad27acc7047
GabriellaValentina92 <gabriella.valentina@outlook.it>
2023-07-14 17:04:10 +0200

    Add call to axios

--- src/App.vue
+++ src/App.vue
@@ -1,9 +1,18 @@
-<script></script>
+<script>
+import projectfolio from "./components/projectfolio.vue";
+
+export default {
+  components: {
+    projectfolio,
+  },
+};
+</script>
 
 <template>
-  <h1>BOOLFOLIO</h1>
+  <!-- <h1>BOOLFOLIO</h1> -->
+  <projectfolio></projectfolio>
 </template>
 
 <style lang="scss">
-@import "../node_modules/bootstrap/scss/bootstrap";
+@import "bootstrap/scss/bootstrap";
 </style>
---
+++ src/components/projectfolio.vue
@@ -0,0 +1,28 @@
+<script>
+import axios from "axios";
+
+export default {
+  data() {
+    return {
+      arrProject: [],
+    };
+  },
+
+  methods: {},
+
+  created() {
+    axios
+      .get("http://localhost:8080/api/projects")
+      .then((response) => (this.arrProject = response.data.data));
+  },
+};
+</script>
+
+<template>
+  <h3>I miei progetti:</h3>
+  <ul>
+    <li v-for="project in arrProject" :key="project.id">{{ project.title }}</li>
+  </ul>
+</template>
+
+<style lang="scss"></style>
